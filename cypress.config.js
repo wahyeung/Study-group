25829f4cd2706f11d8c9ebc960299ed08ec7eb03
Luna Lu <lunalu@Lunas-MacBook-Pro.local>
2024-07-28 02:13:33 -0400

    Complete Challenge 7 tests and fix errors

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  
+  env: {
+    NODE_TLS_REJECT_UNAUTHORIZED: '0' 
+  },
+
   e2e: {
     baseUrl:'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
