22d1e38c6aafebbfa32ec659bd0c1788401a15ca
Luna <104334220+wahyeung@users.noreply.github.com>
2024-07-31 00:14:42 -0400

    Challenge6 branch (#6)
    * Add Cypress E2E tests for Work.co homepage
    
    * Add login tests for SauceDemo
    
    * Add Challenge 5 tests
    
    * Add Challenge 6 tests with Page Object Model
    
    ---------
    
    Co-authored-by: Luna Lu <lunalu@lunas-mbp.mynetworksettings.com>
    Co-authored-by: Luna Lu <lunalu@Lunas-MacBook-Pro.local>

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
-  
-  env: {
-    NODE_TLS_REJECT_UNAUTHORIZED: '0' 
-  },
-
   e2e: {
     baseUrl:'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
