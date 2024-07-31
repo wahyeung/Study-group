acc41be2a63ed644366bf09de97cf9989ac52428
Luna Lu <lunalu@lunas-mbp.mynetworksettings.com>
2024-07-30 23:39:17 -0400

    Refactor cart tests: organize by purpose, use descriptive names, add beforeEach hook

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -4,22 +4,22 @@ describe('Cart Test', () => {
         cy.login('standard_user', 'secret_sauce')
     })
 
-    describe('Test case 1', () => {
-        it('Open checkout and verify errors', () => {
-            cy.get('[data-test="shopping-cart-link"]').click()
+    describe('Test case 1: Verify errors on empty checkout form submission', () => {
+        it('Should display error on empty checkout form submission', () => {
+            cy.get('.shopping_cart_link').click()
             cy.get('[data-test="checkout"]').click()
             cy.get('[data-test="continue"]').click()
             cy.get('[data-test="error"]').should('contain', 'Error: First Name is required')
         })
     })
 
-    describe('Test case 2', () => {
-        it('Verify cart is empty, add item, and complete checkout', () => {
-            cy.get('[data-test ="shopping_cart_badge"]').should('not.exist')
+    describe('Test case 2: Add item to cart and complete checkout', () => {
+        it('Should add item to cart and complete checkout', () => {
+            cy.get('.shopping_cart_badge').should('not.exist')
             cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
             cy.get('.shopping_cart_badge').should('be.visible')
-            cy.get('[data-test="shopping-cart-link"]').click()
-            cy.get('[data-test="inventory-item-name"]').should('contain', 'Sauce Labs Backpack')
+            cy.get('.shopping_cart_link').click()
+            cy.get('.inventory_item_name').should('contain', 'Sauce Labs Backpack')
             cy.get('.cart_quantity').should('contain', '1')
             cy.get('.inventory_item_price').should('contain', '$29.99')
 
@@ -32,18 +32,18 @@ describe('Cart Test', () => {
             cy.get('[data-test="lastName"]').type('Lu')
             cy.get('[data-test="postalCode"]').type('12345')
             cy.get('[data-test="continue"]').click()
-            cy.get('[data-test="inventory-item-name"]').should('contain', 'Sauce Labs Backpack')
+            cy.get('.inventory_item_name').should('contain', 'Sauce Labs Backpack')
             cy.get('[data-test="finish"]').click()
             cy.get('.complete-header').should('contain', 'Thank you for your order')
-            cy.url().should('include', '/checkout-complete.html') 
+            cy.url().should('include', '/checkout-complete.html')
             cy.get('[data-test="back-to-products"]').click()
             cy.url().should('include', '/inventory.html')
-            cy.get('[data-test ="shopping_cart_badge"]').should('not.exist')
+            cy.get('.shopping_cart_badge').should('not.exist')
         })
     })
 
-    describe('Test case 3', () => {
-        it('Add and remove item from cart', () => {
+    describe('Test case 3: Add and remove item from cart', () => {
+        it('Should add and remove item from cart', () => {
             cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
             cy.get('.shopping_cart_badge').should('be.visible').and('contain', '1')
             cy.get('.shopping_cart_link').click()
@@ -52,8 +52,8 @@ describe('Cart Test', () => {
         })
     })
 
-    describe('Test case 4', () => {
-        it('Continue shopping from cart', () => {
+    describe('Test case 4: Continue shopping from cart', () => {
+        it('Should continue shopping from cart', () => {
             cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
             cy.get('.shopping_cart_link').click()
             cy.get('[data-test="continue-shopping"]').click()
@@ -61,6 +61,4 @@ describe('Cart Test', () => {
             cy.get('.shopping_cart_badge').should('be.visible').and('contain', '1')
         })
     })
-
-
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,33 +1,42 @@
-describe('Login Test', ()=> {
-    it('Test case 1', () => {
+describe('Login Test', () => {
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')
-        cy.title().should('eq', 'Swag Labs')
     })
 
-    it('Test case 2', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('[data-test = "username"]').type('123@example.com')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.url().should('eq','https://www.saucedemo.com/')
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
+    describe('Positive Tests', () => {
+        it('Should successfully load the login page', () => {
+            cy.title().should('eq', 'Swag Labs')
+        })
+
+        it('Should successfully login with valid credentials', () => {
+            cy.get('[data-test="username"]').type('standard_user')
+            cy.get('[data-test="password"]').type('secret_sauce')
+            cy.get('[data-test="login-button"]').click()
+            cy.url().should('include', '/inventory.html')
+        })
+
+        it('Should successfully logout', () => {
+     
+            cy.get('[data-test="username"]').type('standard_user')
+            cy.get('[data-test="password"]').type('secret_sauce')
+            cy.get('[data-test="login-button"]').click()
+            cy.url().should('include', '/inventory.html')
+
+         
+            cy.get('#react-burger-menu-btn').click()
+            cy.get('[data-test="logout-sidebar-link"]').click()
+            cy.url().should('eq', 'https://www.saucedemo.com/')
+            cy.get('[data-test="login-button"]').should('be.visible')
+        })
     })
-    
-    it('Test case 3', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('[data-test = "username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.url().should('include', '/inventory.html') 
-    })
-    it('Test case 0', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('[data-test = "username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('#react-burger-menu-btn').click()
-        cy.get('[data-test="logout-sidebar-link"]').click()
-        cy.url().should('eq', 'https://www.saucedemo.com/')
-        cy.get('[data-test="login-button"]').should('be.visible')
+
+    describe('Negative Tests', () => {
+        it('Should display error for invalid login', () => {
+            cy.get('[data-test="username"]').type('123@example.com')
+            cy.get('[data-test="password"]').type('secret_sauce')
+            cy.get('[data-test="login-button"]').click()
+            cy.url().should('eq', 'https://www.saucedemo.com/')
+            cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
+        })
     })
 })
